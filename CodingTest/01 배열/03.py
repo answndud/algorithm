6ce88a00ff2cb208446989cdce16ae565303d2d9
MJY <jmoon0227@gmail.com>
2025-11-04 11:25:12 +0900

    Algorithm - Array 05~06

--- CodingTest/01 배열/03.py
+++ CodingTest/01 배열/03.py
@@ -2,7 +2,7 @@
 
 def solution(numbers):
     answer = []
-    # 두 수를 선택하는 모든 겨웅의 수를 반복문으로 구함
+    # 두 수를 선택하는 모든 경우의 수를 반복문으로 구함
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             # 두 수를 더한 결과를 새로운 배열에 추가
