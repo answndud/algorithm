9cd40421ed4e7ca526225c05195090b9e0a38bd2
MJY <jmoon0227@gmail.com>
2025-12-09 22:09:12 +0900

    Algorithm-Baekjoon-Bronze4-05

--- BaekjoonV2/Bronze4/15873.py
+++ BaekjoonV2/Bronze4/15873.py
@@ -1,7 +1,11 @@
 x = input()
-result = 0
-for i in x:
-    if int(i) == 0:
-        continue
-    result += int(i)
-print(result)
+
+if len(x) == 2:
+    print(sum(map(int, [x[0], x[1]])))
+elif len(x) == 4:
+    print(20)
+else:
+    if int(x[-1]) == 0:		#문자열의 맨 마지막이 0, 즉 B가 10이다
+        print(int(x[0]) + 10)
+    else:			#중간이 0, 즉 A가 10이다
+        print(int(x[-1]) + 10)
